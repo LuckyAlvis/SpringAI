2cf21ae5a9e89c095f3c991d5e7d0c68285b4b55
ivan <252127196@qq.com>
2025-04-01 21:40:20 +0800

    first commit

---
+++ src/main/java/com/shenzhen/dai/dyfspringai/DyfSpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.shenzhen.dai.dyfspringai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DyfSpringAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DyfSpringAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shenzhen/dai/dyfspringai/DyfSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shenzhen.dai.dyfspringai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DyfSpringAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
