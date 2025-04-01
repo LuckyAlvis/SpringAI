48917a3340e7002bbd4e7a4764d7a7e76addf187
ivan <252127196@qq.com>
2025-04-01 22:13:03 +0800

    feature: 实现第一次流式对话

--- src/main/java/com/shenzhen/dai/ai/controller/ChatController.java
+++ src/main/java/com/shenzhen/dai/ai/controller/ChatController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,11 +15,11 @@ public class ChatController {
 
     private final ChatClient chatClient;
 
-    @GetMapping("/chat")
-    public String chat(@RequestParam String prompt) {
+    @GetMapping(value = "/chat", produces = "text/html;charset=utf8")
+    public Flux<String> chat(@RequestParam String prompt) {
         return chatClient.prompt()
                 .user(prompt)
-                .call()
+                .stream()
                 .content();
     }
 }
