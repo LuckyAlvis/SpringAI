e69352002a8171a8db91981aed5c13f1b4149cb3
ivan <252127196@qq.com>
2025-04-02 09:37:54 +0800

    feature: 实现内存存储会话记忆

--- src/main/java/com/shenzhen/dai/ai/config/CommonConfiguration.java
+++ src/main/java/com/shenzhen/dai/ai/config/CommonConfiguration.java
@@ -1,7 +1,10 @@
 package com.shenzhen.dai.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,13 +16,22 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class CommonConfiguration {
+
+    @Bean
+    public ChatMemory chatMemory() {
+        return new InMemoryChatMemory();
+    }
+
     @Bean
     public ChatClient chatClient(OllamaChatModel model) {
         return ChatClient
                 .builder(model)
                 .defaultSystem("你的名字叫贾维斯")
                 // 增加日志拦截器
-                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor(),
+                        new MessageChatMemoryAdvisor(chatMemory())
+                )
                 .build();
 
     }
